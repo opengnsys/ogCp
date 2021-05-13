d84933f88ec57bf495da9a93ad7caa7adcda1aeb
OpenGnSys Support Team <soporte-og@soleta.eu>
2021-05-14 00:32:45 +0200

    ogcp is AGPLv3+
    Update license header in files.

--- ogcp/forms/action_forms.py
+++ ogcp/forms/action_forms.py
@@ -1,3 +1,10 @@
+# Copyright (C) 2020-2021 Soleta Networks <info@soleta.eu>
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+
 from wtforms import (
     Form, SubmitField, HiddenField, SelectField, BooleanField, IntegerField,
     StringField, RadioField
